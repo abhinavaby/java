a5b308ce31731de20e0f951d4b4e6de156559d9d
abhinavaby <abhinavaby07@gmail.com>
2026-07-04 22:46:41 +0530

    LOGICAL OPERATORS

---
+++ src/logical_operators.java
@@ -0,0 +1,9 @@
+import java.lang.classfile.attribute.AnnotationDefaultAttribute;
+
+public class logical_operators {
+    public static void main(String[] args) {
+//        && = AND;
+//        || = OR;
+//        ! = NOT
+    }
+}
