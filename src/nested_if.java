e45893269bcbd307191e06e77e41856c85ffd4c6
abhinavaby <abhinavaby07@gmail.com>
2026-07-04 15:25:03 +0530

    nested if

--- src/nested_if.java
+++ src/nested_if.java
@@ -1,2 +1,26 @@
 public class nested_if {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 40;
+        int c = 30;
+        int greater=0;
+
+        if(a>b){
+            if(a>c){
+                greater = a;
+
+            }
+            else {
+                greater = c;
+            }
+
+        }
+        else if(b>c){
+            greater = b;
+        }
+        else{
+            greater = c;
+        }
+        System.out.println("greater = "+ greater);
+    }
 }
