26859d861ef09cc13f44a5b402ef3557435f8d8a
abhinavaby <abhinavaby07@gmail.com>
2026-07-04 22:35:24 +0530

    ternary

---
+++ src/ternary.java
@@ -0,0 +1,13 @@
+public class ternary {
+    public static void main(String[] args) {
+        int score = 20;
+        String point = score>50 ? "high" : "low";
+        System.out.println(point);
+
+        System.out.println(" ");
+
+        int num = 32;
+        String out = (num%2==0)? "even" : "odd";
+        System.out.println(out);
+    }
+}
