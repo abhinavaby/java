297c38650db2af915876e3b3f70b465d72792ce5
abhinavaby <abhinavaby07@gmail.com>
2026-07-04 07:45:13 +0530

    math

--- src/math.java
+++ src/math.java
@@ -2,5 +2,6 @@ public class math {
     public static void main(String[] args) {
         System.out.println(Math.PI);
         System.out.println(Math.E);
+        System.out.println(Math.cos(Math.PI));
     }
 }
