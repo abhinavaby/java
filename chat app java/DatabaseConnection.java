21d66dafeb643b747c7587887413fc04c519413d
abhinavaby <abhinavaby07@gmail.com>
2026-07-14 14:55:27 +0530

    NIL
    Updated the database password placeholder for security.

--- chat app java/DatabaseConnection.java
+++ chat app java/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/ChatApp";
     private static final String USER = "root";
-    private static final String PASSWORD = "Appu123kh#"; // Change to your MySQL password
+    private static final String PASSWORD = "#######"; // Change to your MySQL password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
